5f376a5a748a54b850e61f7439da4501fe9d2bf8
Anton <mail.anton88@gmail.com>
2023-06-02 22:07:52 +0300

    new file

--- src/screens/postsScreen/PostsScreen.js
+++ src/screens/postsScreen/PostsScreen.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { View, Text, Button, Image, FlatList } from "react-native";
-import { getPosts } from "../../utils/service";
+import { getPosts, deletePost } from "../../utils/service";
 import styles from "./styles";
 
 export default function PostsScreen() {
@@ -15,22 +15,32 @@ export default function PostsScreen() {
     }
   };
 
+  const handleDeletePost = async (id) => {
+    try {
+      await deletePost(id);
+      fetchPosts();
+    } catch (error) {
+      console.error(`Ошибка при удалении поста с ID=${id}: `, error);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Button title="Получить посты" onPress={fetchPosts} />
-      {posts && (
-        <FlatList
-          data={posts}
-          keyExtractor={(item) => item.id.toString()}
-          renderItem={({ item }) => (
-            <View style={styles.post}>
-              <Text style={styles.title}>{item.title}</Text>
-              <Text style={styles.text}>{item.text}</Text>
-              <Image source={{ uri: item.image }} style={styles.image} />
-            </View>
-          )}
-        />
-      )}
-    </View>
+      <View style={styles.container}>
+        <Button title="Получить посты" onPress={fetchPosts} />
+        {posts && (
+            <FlatList
+                data={posts}
+                keyExtractor={(item) => item.id.toString()}
+                renderItem={({ item }) => (
+                    <View style={styles.post}>
+                      <Text style={styles.title}>{item.title}</Text>
+                      <Text style={styles.text}>{item.text}</Text>
+                      <Image source={{ uri: item.image }} style={styles.image} />
+                      <Button title="Удалить пост" onPress={() => handleDeletePost(item.id)} />
+                    </View>
+                )}
+            />
+        )}
+      </View>
   );
 }
